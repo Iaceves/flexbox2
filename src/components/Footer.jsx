83aef17447bc413b14c45ed89689408f61efc965
jmasterr <jaim.toledo10@gmail.com>
2024-02-01 14:36:17 -0600

    finished adding components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import '../App.css'
+
+function Footer() {
+    return(
+        <footer>
+          <div>
+            <h1>created by Ismael 2024</h1>
+          </div>
+          <div>
+            <a>Facebook</a>
+            <a>Instagram</a>
+            <a>Youtube</a>
+          </div>
+        </footer>
+    )
+}
+export default Footer
