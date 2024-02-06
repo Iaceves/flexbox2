8bd0bcb6130b411abe484df0b8b5436f1c81695c
Ismael Aceves <ismael@Ismaels-MBP.lan1>
2024-02-06 16:05:41 -0500

    added login to the login form inputs

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,19 +1,29 @@
+import { useState } from 'react';
 import '../App.css'
 
 function LoginPage(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function onPress(){
+        console.log("I have been Click!!!")
+        console.log(email, password)
+    }
+
+
     return(
         <div className='login-container'>
             <h1>Login</h1>
             <div>
                 <h1>Email:</h1>
-                <input name='email' placeholder="Email" />
+                <input placeholder="Email" type="text" value={email} onChange={e => setEmail(e.target.value)} />
             </div>
             <div>
                 <h1>Password:</h1>
-                <input name="password" placeholder="Password" />
+                <input placeholder="Password" type='password' value={password} onChange={e => setPassword(e.target.value)} />
             </div>
             <div>
-                <button>Login</button>
+                <button onClick={onPress}>Login</button>
                 <button>Signup</button>
             </div>
         </div>
